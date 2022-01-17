3d8fb01418ee8b0242921e8c3038b5523b459e5f
Hosung Park <indra622@gmail.com>
2022-01-17 20:34:08 +0900

    ex31

---
+++ ex31.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+bool sol(int arr[], int length){
+
+    for (int i = 0; i < length-2; i++){
+        if(arr[i]==1 && arr[i+1]==2 && arr[i+2] == 3){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+
+int main(void){
+    int arr[] = {1,1,2,3,1};
+    int arr2[] = {1,1,2,4,1};
+    int arr3[] = {1,1,2,1,2,3};
+    int len1 = sizeof(arr) / sizeof(arr[0]);
+    int len2 = sizeof(arr2) / sizeof(arr2[0]);
+    int len3 = sizeof(arr3) / sizeof(arr3[0]);
+    cout << sol(arr, len1) << endl;
+    cout << sol(arr2, len2) << endl;
+    cout << sol(arr3, len3) << endl;
+    
+    return 0;
+}
